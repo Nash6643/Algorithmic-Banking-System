4515eecbfec25421566ccfeb934c4243c4aa7d6a
Nash6643 <omarnash619@gmail.com>
2026-01-05 14:45:00 +0300

    refactor(button): parameterize corner arc dimensions for rendering

--- Project/src/bankingGUI/ModernButton.java
+++ Project/src/bankingGUI/ModernButton.java
@@ -6,6 +6,7 @@ import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class ModernButton extends JButton {
+    private static final int CORNER_ARC = 15;
 
     private Color normalColor;
     private Color hoverColor;
@@ -64,19 +65,12 @@ public class ModernButton extends JButton {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Subtle drop shadow on hover
-        if (isHovered) {
-            g2.setColor(new Color(0, 0, 0, 40));
-            g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, 15, 15);
-        }
-
         g2.setColor(getBackground());
-        g2.fillRoundRect(0, 0, getWidth() - (isHovered ? 2 : 0), getHeight() - (isHovered ? 2 : 0), 15, 15);
-
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), CORNER_ARC, CORNER_ARC);
+        super.paintComponent(g2);
         g2.dispose();
-        super.paintComponent(g);
     }
+
 
     @Override
     protected void paintBorder(Graphics g) {
